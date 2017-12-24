2cdcee3df543ed622979a44de972545af2306e1d
Ali <eli.enwer@gmail.com>
2017-12-24 13:25:08 +0800

    Six DOF Features: The solution needs updation

--- viper850_plot.cpp
+++ viper850_plot.cpp
@@ -70,12 +70,16 @@ std::vector<double> get_features (vpFeaturePoint p[4])
   double c = fabs (p[3].get_y() - p[0].get_y());
   double d = fabs (vec2Y);
 
-  double omega_x = log (c / d);
-  double omega_y = log (a / b);
+  // double omega_x = log (c / d);
+  // double omega_y = log (a / b);
+
+  double omega_x = c / d;
+  double omega_y = a / b;
+
 
   final.push_back(u); final.push_back (v);
-  final.push_back(Area); final.push_back (omega_x);
-  final.push_back(omega_y); final.push_back(angle);
+  final.push_back(Area); final.push_back (omega_y);
+  final.push_back(omega_x); final.push_back(angle);
 
   return final;
 }
